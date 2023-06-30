6d8e5c5f0383bf3dc6c8351f802e839b92fd9956
StruzhakS <StruzhakS@gmail.com>
2023-06-30 22:34:54 +0300

    bugdixed 4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import Tweet from './Components/Tweet/Tweet';
-import s from './App.module.css';
+// import s from './App.module.css';
 
 function App() {
   return (
-    <div className={s.generalBox}>
+    <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tweet" element={<Tweet />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
